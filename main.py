b8d9be35943885f970cf4e5f733308df90f7312d
Takashi Sasaki <t.sasaki@bmandw.com>
2012-05-11 18:19:21 +0900

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/home/t_sasaki/webapp2/env/bin/python
+
+import webapp2
+
+class HelloWebapp2(webapp2.RequestHandler):
+  def get(self):
+    self.response.write("Hello, webapp2!")
+
+app = webapp2.WSGIApplication([
+  ('/', HelloWebapp2),
+  ], debug=True)
+
+def main():
+  from paste import httpserver
+  httpserver.serve(app, host='127.0.0.1', port='8080')
+
+if __name__ == '__main__':
+  main()
+
+
