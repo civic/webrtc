8b7682507a9a527998ccd3770edff7bdc63ac03b
Takashi Sasaki <tsskmail@gmail.com>
2012-05-18 00:55:39 +0900

    jinja template

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 #!env/bin/python
 
+import os
 import webapp2
+from webapp2_extras import json
+from webapp2_extras import jinja2
 
-class HelloWebapp2(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler):
+  @webapp2.cached_property
+  def jinja2(self):
+    return jinja2.get_jinja2(app=self.app)
+
+  def render_response(self, _tempplate, **context):
+    path = os.path.join(os.path.dirname(__file__), _tempplate)
+    rv = self.jinja2.render_template(path, **context)
+    self.response.write(rv)
+
+class HelloWebapp2(BaseHandler):
   def get(self):
-    self.response.write("Hello, webapp2!")
+    self.response.write("Hello, webapp2!!!")
+    self.response.write(json.encode({"hello": "World"}))
+
+    context = {"message": "Hello WOrld"}
+    self.render_response("template.html", **context)
+
 
 app = webapp2.WSGIApplication([
   ('/', HelloWebapp2),
